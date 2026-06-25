bc947a0577a5fed61912affe2dd58747169c79ec
Nick <kolya.strepetov@mail.ru>
2026-06-25 18:52:10 +0500

    feat: add character selection screen placeholder

--- menu.gd
+++ menu.gd
@@ -9,4 +9,4 @@ func _on_quit_pressed() -> void:
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://level.tscn")
+	get_tree().change_scene_to_file("res://char_selection.tscn")
